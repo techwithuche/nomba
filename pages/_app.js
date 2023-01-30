e875da285a5bb42430263bd66d3a229be167ad91
Uche George <67732362+techwithuche@users.noreply.github.com>
2023-01-30 09:17:40 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import App from "next/app";
 import React from "react";
 import Layout from "../src/assets/Layout";
-import '../src/styles/bootstrap.css';
-import '../src/styles/main.css';
+import '../src/styles/main.css'
+
 
 
 class MyApp extends App {
